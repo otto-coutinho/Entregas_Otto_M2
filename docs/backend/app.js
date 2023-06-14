e1bf141d9a48aeba1b68ee56b81b86f42b57784e
Otto Lima <otto.lima@sou.inteli.edu.br>
2023-06-13 23:11:13 -0300

    int back-front -- AJAX requisition

---
+++ docs/backend/app.js
@@ -0,0 +1,36 @@
+const express = require('express'); 
+const bodyParser = require('body-parser');
+const urlencodedParser = bodyParser.urlencoded({ extended: false })
+
+const sqlite3 = require('sqlite3').verbose();
+const DBPATH = '../data/dbUser.db';
+
+const hostname = '127.0.0.1';
+const port = 2023;
+const app = express();
+
+/* Colocar toda a parte estática no frontend */
+app.use(express.static("../frontend/"));
+
+/* Definição dos endpoints */
+/******** CRUD ************/
+app.use(express.json());
+
+// Retorna todos registros da tabela formacao (é o R do CRUD - Read)
+app.get('/formacoes', (req, res) => {
+	res.statusCode = 200;
+	res.setHeader('Access-Control-Allow-Origin', '*');
+	var db = new sqlite3.Database(DBPATH); // Abre o banco
+	var sql = 'SELECT * FROM formacao WHERE id = 1';
+		db.all(sql, [],  (err, rows ) => {
+			if (err) {
+				throw err;
+			}
+			res.json(rows);
+		});
+		db.close(); // Fecha o banco
+});
+
+app.listen(port, hostname, () => {
+	console.log(`Servidor rodando em http://${hostname}:${port}/`);
+  });
